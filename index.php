782a301443136764610ecf5038170574d53b9979
Mainul Dip <mainuldip@gmail.com>
2021-06-26 02:05:41 +0600

    Initial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name:       wp-block-extend-style
+ * Description:       Extending Core Block Styless
+ * Requires at least: 5.7
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            MainulDip
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-block-extend-style-lg
+ *
+ * @package           create-block
+ */
+
+
+
+// core/group
+
+define('TXTDOM', 'wp-block-extend-style-lg');
+
+// register_block_style('core/group', array('name' => 'custom-border', 'label' => __('Custom Borders', 'TXTDOM')));
